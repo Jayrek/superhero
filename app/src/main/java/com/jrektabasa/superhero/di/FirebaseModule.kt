94e8f0e405f1bffe850d1bd1b7a31486dc46cc77
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-11 15:50:57 +0800

    add firebase to hilt dependency

---
+++ app/src/main/java/com/jrektabasa/superhero/di/FirebaseModule.kt
@@ -0,0 +1,25 @@
+package com.jrektabasa.superhero.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun providesFirestoreInstance(): FirebaseFirestore = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun providesFirebaseAuthInstance(): FirebaseAuth = Firebase.auth
+}
