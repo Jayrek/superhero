c4db3cbb0d9262c2ec8ef2ddc2e5b7f39e141359
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-11 15:50:34 +0800

    add firebase fields and collections

---
+++ app/src/main/java/com/jrektabasa/superhero/util/firebase/FirebaseCollections.kt
@@ -0,0 +1,7 @@
+package com.jrektabasa.superhero.util.firebase
+
+object FirebaseCollections {
+    const val USER = "user"
+    const val HERO = "hero"
+    const val HERO_ANALYSIS = "hero_analysis"
+}
