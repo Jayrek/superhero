d416ff3b0bf00dd75676d305476b7daea09c572c
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-16 21:24:17 +0800

    update auth remote data source

--- app/src/main/java/com/jrektabasa/superhero/data/remote/data_source/auth/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/jrektabasa/superhero/data/remote/data_source/auth/AuthRemoteDataSourceImpl.kt
@@ -1,11 +1,16 @@
 package com.jrektabasa.superhero.data.remote.data_source.auth
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import android.content.Intent
+import android.content.IntentSender
+import android.util.Log
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.jrektabasa.superhero.R
-import com.jrektabasa.superhero.presentation.state.SignInUiState
+import com.jrektabasa.superhero.BuildConfig
+import com.jrektabasa.superhero.data.common.Result
+import com.jrektabasa.superhero.data.model.response.UserResponse
 import kotlinx.coroutines.tasks.await
 import java.lang.Exception
 import javax.inject.Inject
@@ -13,19 +18,62 @@ import javax.inject.Singleton
 
 @Singleton
 class AuthRemoteDataSourceImpl @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val oneTapClient: SignInClient
 ) :
     AuthRemoteDataSource {
-    override suspend fun signInWithGoogle(idToken: String): SignInUiState {
-        return try {
-            val credential = GoogleAuthProvider.getCredential(idToken, null)
-            val auth = firebaseAuth.signInWithCredential(credential).await()
-            val user = auth.user
 
-            SignInUiState(user= user, isSuccess = true)
+    override suspend fun signInWithGoogle(): Result<IntentSender?> {
+        return try {
+            val result = oneTapClient.beginSignIn(buildSignInRequest()).await()
+            Result.Success(result?.pendingIntent?.intentSender)
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
+    }
+
+    override suspend fun signInWithIntent(intent: Intent): Result<UserResponse> {
+        val intentCredential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val idToken = intentCredential.googleIdToken
+        val googleCredential = GoogleAuthProvider.getCredential(idToken, null)
+        return try {
+            val user = firebaseAuth.signInWithCredential(googleCredential).await().user
+            Log.wtf("USER: ", user?.displayName.toString())
+            Result.Success(
+                UserResponse(
+                    userId = user?.uid,
+                    userName = user?.displayName,
+                    userProfileUrl = user?.photoUrl.toString()
+                )
+            )
 
         } catch (e: Exception) {
-            SignInUiState(isSuccess = false, error = e.message ?: "Unknown error occurred!")
+            Result.Error(e.message.toString())
+        }
+    }
+
+
+    private fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+
+    override suspend fun signOut(): Result<Unit> {
+        return try {
+            oneTapClient.signOut().await()
+            firebaseAuth.signOut()
+            Result.Success(Unit)
+        }
+        catch (e: Exception){
+            Result.Error(e.message.toString())
         }
     }
 }
