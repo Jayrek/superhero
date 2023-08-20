8ca1c025005b901dab87224f856d0e1df77f58cc
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-20 23:12:40 +0800

    add view model for fetching hero by id from hero api

---
+++ app/src/main/java/com/jrektabasa/superhero/presentation/viewmodel/hero/HeroByIdViewModel.kt
@@ -0,0 +1,28 @@
+package com.jrektabasa.superhero.presentation.viewmodel.hero
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jrektabasa.superhero.data.common.Result
+import com.jrektabasa.superhero.domain.model.HeroApi
+import com.jrektabasa.superhero.domain.use_case.hero.FetchHeroApiByIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeroByIdViewModel @Inject constructor(
+    private val useCase: FetchHeroApiByIdUseCase
+) : ViewModel() {
+
+    private val _hero = MutableStateFlow<Result<HeroApi>?>(null)
+    val hero = _hero.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val response = useCase.execute("720")
+            _hero.value = response
+        }
+    }
+}
