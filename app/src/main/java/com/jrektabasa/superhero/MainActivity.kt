4f1b327a3b829cc9e99ba042fd83deaa4cdf4257
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-21 00:40:28 +0800

    add nav host controller for the screen navigation of the app

--- app/src/main/java/com/jrektabasa/superhero/MainActivity.kt
+++ app/src/main/java/com/jrektabasa/superhero/MainActivity.kt
@@ -18,6 +18,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.jrektabasa.superhero.data.common.Result
 import com.jrektabasa.superhero.presentation.screen.DashBoardScreen
 import com.jrektabasa.superhero.presentation.screen.HeroScreen
@@ -27,45 +35,84 @@ import com.jrektabasa.superhero.presentation.viewmodel.biography.BiographyViewMo
 import com.jrektabasa.superhero.presentation.viewmodel.hero.HeroByIdViewModel
 import com.jrektabasa.superhero.presentation.viewmodel.hero.HeroViewModel
 import com.jrektabasa.superhero.ui.theme.SuperheroTheme
+import com.jrektabasa.superhero.util.navigation.NavigationScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+
+@Composable
+fun HeroApp(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = NavigationScreen.DashBoard.route
+) {
+
+    val heroViewModel: HeroViewModel = viewModel()
+    val heroByIdViewModel: HeroByIdViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(NavigationScreen.DashBoard.route) {
+            DashBoardScreen(navHostController = navController, heroViewModel)
+        }
+
+        composable(
+            NavigationScreen.Hero.route,
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) { backStackEntry ->
+            backStackEntry.arguments?.getString("id")?.let { id ->
+                HeroScreen(
+                    navHostController = navController,
+                    viewModel = heroByIdViewModel,
+                    id = id
+                )
+            }
+
+        }
+    }
+
+}
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val biographyViewModel by viewModels<BiographyViewModel>()
-    private val authViewModel by viewModels<AuthViewModel>()
+
+//    val heroViewModel by viewModels<HeroViewModel>()
+//    val heroByIdViewModel by viewModels<HeroByIdViewModel>()
+
+//    private val biographyViewModel by viewModels<BiographyViewModel>()
+//    private val authViewModel by viewModels<AuthViewModel>()
 
 //    private val heroViewModel by viewModels<HeroViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val heroViewModel by viewModels<HeroViewModel>()
-            val authViewModel by viewModels<AuthViewModel>()
-            val heroByIdViewModel by viewModels<HeroByIdViewModel>()
+
+            /* val authViewModel by viewModels<AuthViewModel>()
+             val heroByIdViewModel by viewModels<HeroByIdViewModel>()
 
 
-            val signInIntent = authViewModel.intentSender.collectAsState()
+             val signInIntent = authViewModel.intentSender.collectAsState()
 
-            val heroList = heroViewModel.heroList.collectAsState()
+             val heroList = heroViewModel.heroList.collectAsState()
 
-            val googleSignInLauncher = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.StartIntentSenderForResult(),
-                onResult = { result ->
-                    if (result.resultCode == RESULT_OK) {
-                        lifecycleScope.launch {
-                            authViewModel.signInWithIntent(result)
+             val googleSignInLauncher = rememberLauncherForActivityResult(
+                 contract = ActivityResultContracts.StartIntentSenderForResult(),
+                 onResult = { result ->
+                     if (result.resultCode == RESULT_OK) {
+                         lifecycleScope.launch {
+                             authViewModel.signInWithIntent(result)
 
-                        }
-//                    val intent = it.data
-//                    handleGoogleSignInState(intent)
-                    }
-                }
-            )
+                         }
+ //                    val intent = it.data
+ //                    handleGoogleSignInState(intent)
+                     }
+                 }
+             )*/
 
             SuperheroTheme {
-                DashBoardScreen(heroViewModel)
+                HeroApp()
+//                DashBoardScreen(heroViewModel)
 //                HeroScreen(heroByIdViewModel)
 //                SignInScreen(authViewModel)
                 // A surface container using the 'background' color from the theme
