fb24b68c60d18b98e2b49729a5ff3f69fccb82ce
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-08-20 23:12:06 +0800

    fix hero api response using serialize name annotation for powerstats

--- app/src/main/java/com/jrektabasa/superhero/data/model/response/HeroApiResponse.kt
+++ app/src/main/java/com/jrektabasa/superhero/data/model/response/HeroApiResponse.kt
@@ -1,5 +1,6 @@
 package com.jrektabasa.superhero.data.model.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -10,6 +11,7 @@ data class HeroApiResponse(
     val id: String,
     val image: ImageResponse,
     val name: String,
+    @SerialName("powerstats")
     val powerStats: PowerStatsResponse,
     val response: String,
     val work: WorkResponse
